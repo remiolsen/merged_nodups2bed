e9408356ba68f35f9fa2a72b6f2e50a570c43e33
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2020-02-10 17:05:46 +0100

    use sort not gsort

--- merged_nodups2bed.sh
+++ merged_nodups2bed.sh
@@ -23,7 +23,7 @@ fi
 if hash parallel 2>/dev/null; then
   export readlen
   parallel -j $awk_t --block -1 --pipepart -a $infile awk \'{if\(\$1==0\){r1strand=\"+\"}else{r1strand=\"-\"}\;if\(\$5==0\){r2strand=\"+\"}else{r2strand=\"-\"}\;print \$2,\$3,\$3+\'\$readlen\',\$15,\$9,r1strand\;print \$6,\$7,\$7+\'\$readlen\',\$16,\$12,r2strand}\' | \
-  gsort -k 4 --parallel=$sort_t > $outfile
+  sort -k 4 --parallel=$sort_t > $outfile
 else
   awk '{
         if($1==0){r1strand="+"}else{r1strand="-"};
