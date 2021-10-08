5cf9d8fbbd884153a43099bc0f052e248d872288
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2021-10-08 15:35:20 +0200

    Automatically appends /1 and /2 to read names without them

--- merged_nodups2bed.sh
+++ merged_nodups2bed.sh
@@ -15,6 +15,8 @@ while getopts ":i:o:" opt; do
   esac
 done
 
+ma=`awk '{print match($15, "/")}' $infile | head -n 1`
+
 shift $((OPTIND-1))
 if [ -z "${infile}" ] || [ -z "${outfile}" ]; then
     usage
@@ -22,13 +24,16 @@ fi
 
 if hash parallel 2>/dev/null; then
   export readlen
-  parallel -j $awk_t --block -1 --pipepart -a $infile awk \'{if\(\$1==0\){r1strand=\"+\"}else{r1strand=\"-\"}\;if\(\$5==0\){r2strand=\"+\"}else{r2strand=\"-\"}\;print \$2,\$3,\$3+\'\$readlen\',\$15,\$9,r1strand\;print \$6,\$7,\$7+\'\$readlen\',\$16,\$12,r2strand}\' | \
+  export ma
+  parallel -j $awk_t --block -1 --pipepart -a $infile awk \'{if\(\$1==0\){r1strand=\"+\"}else{r1strand=\"-\"}\;if\(\$5==0\){r2strand=\"+\"}else{r2strand=\"-\"}\;if\(\'\$ma\' \> 0\){r1=\$15}else{r1=\$15\"/1\"}\;if\(\'\$ma\' \> 0\){r2=\$16}else{r2=\$16\"/2\"}\;print \$2,\$3,\$3+\'\$readlen\',r1,\$9,r1strand\;print \$6,\$7,\$7+\'\$readlen\',r2,\$12,r2strand}\' | \
   sort -k 4 --parallel=$sort_t > $outfile
 else
   awk '{
         if($1==0){r1strand="+"}else{r1strand="-"};
         if($5==0){r2strand="+"}else{r2strand="-"};
-        print $2,$3,$3+'$readlen',$15,$9,r1strand;
-        print $6,$7,$7+'$readlen',$16,$12,r2strand
-      }' < $infile | sort -k 4 > $outfile
+        if('$ma' > 0){r1=$15}else{r1=$15"/1"};
+        if('$ma' > 0){r2=$16}else{r2=$16"/2"};
+        print $2,$3,$3+'$readlen',r1,$9,r1strand;
+        print $6,$7,$7+'$readlen',r2,$12,r2strand
+      }' $infile | sort -k 4 > $outfile
 fi
